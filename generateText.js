6569d140894eb545e47f52c8ec66864e82ed2d48
UndersunIO <alex.atc.digital@gmail.com>
2021-08-31 22:40:48 +0200

    Exercice generer une phrase + affichage initial

---
+++ generateText.js
@@ -0,0 +1,6 @@
+let nom = "Undersun";
+let age = 25;
+
+let phrase = "Bonjour " + nom + " tu as " + age + " ans";
+
+console.log(phrase);
---
+++ initials.js
@@ -0,0 +1,8 @@
+let pseudo = "Undersun";
+let nameTag = "SunIO";
+let initals = pseudo[0] + nameTag[0];
+
+let resultat = [];
+
+resultat.push(pseudo, nameTag, initals);
+console.log(resultat);
